7ef5537f055300ea431d9738ac62b2e10e32a661
Mathias Rav <rav@cs.au.dk>
2016-05-30 12:34:35 +0200

    Add LockstepConsumers

--- pysnake.py
+++ pysnake.py
@@ -52,6 +52,53 @@ def main(stdscr):
         async def __anext__(self):
             return await getch()
 
+    class LockstepConsumer:
+        def __init__(self, f):
+            self.f = f
+
+        async def __aiter__(self):
+            return self
+
+        async def __anext__(self):
+            return await self.f()
+
+    class LockstepConsumers:
+        def __init__(self):
+            self.consumers = 0
+            self.futures = []
+            self._waiter = None
+
+        async def consume(self, it):
+            async for v in it:
+                await self.push(v)
+
+        def consumer(self):
+            self.consumers += 1
+            return LockstepConsumer(self._consume_next)
+
+        async def _consume_next(self):
+            f = asyncio.Future()
+            self.futures.append(f)
+            self._notify()
+            return await f
+
+        def _notify(self):
+            if self._waiter:
+                self._waiter.set_result(None)
+                self._waiter = None
+
+        async def _wait(self):
+            f = asyncio.Future()
+            self._waiter = f
+            await self._waiter
+
+        async def push(self, v):
+            while len(self.futures) < self.consumers:
+                await self._wait()
+            for f in self.futures:
+                f.set_result(v)
+            self.futures = []
+
     addch = complex_wrap(stdscr.addch)
     move = complex_wrap(stdscr.move)
     inch = complex_wrap(stdscr.inch)
@@ -159,11 +206,13 @@ def main(stdscr):
             refresh()
             await asyncio.sleep(0.1)
 
+    input = LockstepConsumers()
     loop = asyncio.get_event_loop()
     tasks = [
+        asyncio.ensure_future(input.consume(CursesCharacters())),
         asyncio.ensure_future(food_loop(5+5j)),
         asyncio.ensure_future(
-            the_snake.get_directions(CursesCharacters())),
+            the_snake.get_directions(input.consumer())),
         asyncio.ensure_future(play([the_snake])),
     ]
     try:
